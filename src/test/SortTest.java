1634c4621b8a2c4b7dada69a3a94bf6e0f8ce3e8
Christopher McIntyre <chrsintyre@gmail.com>
2016-02-14 13:18:52 +0000

    Added reverse selection sort

--- src/test/SortTest.java
+++ src/test/SortTest.java
@@ -16,10 +16,26 @@ public class SortTest {
 	}
 	
 	@Test
+	public void selectionReverseSortShouldSortIntArray() {
+		int[] arr = {5, 7, 2, 3, 5};
+		int[] expected = {7, 5, 5, 3, 2};
+        Selection.reverseSort(arr);
+        assertArrayEquals(expected, arr);
+	}
+	
+	@Test
 	public void selectionSortShouldSortStringArray() {
 		String[] arr = {"I", "am", "an", "array", "lol"};
 		String[] expected = {"am", "an", "array", "I", "lol"};
 		Selection.sort(arr);
+        assertArrayEquals(expected, arr);
+	}
+
+	@Test
+	public void selectionReverseSortShouldSortStringArray() {
+		String[] arr = {"I", "am", "an", "array", "lol"};
+		String[] expected = {"lol", "I", "array", "an", "am"};
+		Selection.reverseSort(arr);
         assertArrayEquals(expected, arr);
 	}
 }
